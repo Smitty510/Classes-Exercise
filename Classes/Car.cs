5040b4727cd6c7e150967e2ed343d509f5c1840c
patrick smith <smithpatrick.510@gmail.com>
2023-07-01 15:02:07 -0400

    done

---
+++ Classes/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Car
+    {
+        //make three properties
+        //one for make
+        public string Make { get; set; }
+
+        ////one for model
+        public string Model { get; set; }
+
+        /////one for year
+        public int Year { get; set; }
+
+
+    }
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -2,9 +2,14 @@
 {
     public class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+         static void Main(string[] args)
+         {
+            Car myCar = new Car();
+            myCar.Make = "Chevrolet";
+            myCar.Model = "Tahoe";
+            myCar.Year = 2018;
+
+            Console.WriteLine($"My new car is a {myCar.Year} {myCar.Make} {myCar.Model}");
+         }
     }
 }
